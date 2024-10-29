23e0f299098a5a3e080e9de06c2b9e4d10088a3d
“Tiara <tiaravan234@gmail.com>
2024-10-29 10:59:15 -0400

    added event listeners

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ document.querySelectorAll("a").forEach((link) => {
     chrome.tabs.create({ url: link.href });
   });
 });
+
+const orderedList = document.getElementById("shortcut-list");
+let draggedItem = null;
+
+orderedList.addEventListener("dragstart", (event) => {
+  draggedItem = event.target;
+  event.target.classList.add("dragging");
+});
+
+orderedList.addEventListener("dragend", (event) => {
+  // reset the transparency
+  event.target.classList.remove("dragging");
+  draggedItem = null;
+});
+
